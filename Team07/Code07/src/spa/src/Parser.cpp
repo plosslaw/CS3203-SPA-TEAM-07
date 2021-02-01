2800e28f7e4ce9996720cc8639d168905978a218
AHaliq <liqo12@hotmail.com>
2021-02-01 18:39:17 +0800

    read filename and add test for ParseState

--- Team07/Code07/src/spa/src/Parser.cpp
+++ Team07/Code07/src/spa/src/Parser.cpp
@@ -2,13 +2,43 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <streambuf>
 
-using namespace std;
+//using namespace std;
 
 #include "PKB.h"
 #include "TNode.h"
 
-int Parse () {
+template<typename T>
+class ParseState {
+		T a;
+		int o;
+		std::string* str;
+	public:
+		ParseState<T>(ParseState<T> &p) {
+			str = p.str;
+			o = p.o;
+			a = p.a;
+		}
+		ParseState(std::string &s) {
+			str = &s;
+			o = 0;
+		}
+		char head() {
+			return (*str).at(o++);
+		}
+		bool end() {
+			return (*str).length() == o;
+		}
+};
 
+int Parse (string file) {
+	std::ifstream t(file);
+	std::string str((std::istreambuf_iterator<char>(t)),
+                 std::istreambuf_iterator<char>());
+	ParseState<char> p(str);
+	cout<<p.head()<<"...\n\n\n";
+	cout<<str<<"\n\n\n\n";
 	return 0;
 }
