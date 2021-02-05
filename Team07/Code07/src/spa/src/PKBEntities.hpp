4789b78ea8bdd80dfe293fac9039da0d18e241a6
Pakorn Ueareeworakul <pakorn.ue@gmail.com>
2021-02-05 22:12:04 +0800

    Add types for PKB entities

---
+++ Team07/Code07/src/spa/src/PKBEntities.hpp
@@ -0,0 +1,23 @@
+#include "Types.hpp"
+#include "TNode.h"
+#include <unordered_set>
+#include <optional>
+
+typedef struct {
+    stmt_ref reference;
+    stmt_type type;
+    TNode ast;
+    std::unordered_set<var_ref> uses;
+    std::unordered_set<var_ref> modifies;
+
+    std::optional<stmt_ref> parent;
+    std::optional<stmt_ref> follows;
+
+    std::unordered_set<stmt_ref> indirect_parent;
+    std::unordered_set<stmt_ref> indirect_follows;
+
+} statement;
+
+typedef struct {
+    var_ref reference;
+} variable;
