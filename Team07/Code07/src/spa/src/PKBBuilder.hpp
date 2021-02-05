4174286da624534e5285cb82748f30eaeec55770
Pakorn Ueareeworakul <pakorn.ue@gmail.com>
2021-02-05 22:13:23 +0800

    Add class definition for PKBBuilder
    which is equivalent to design extractor

---
+++ Team07/Code07/src/spa/src/PKBBuilder.hpp
@@ -0,0 +1,14 @@
+#include "PKBEntities.hpp"
+#include "PKB.h"
+
+class PKBBuilder {
+    private:
+        PKB pkb;
+        TNode ast_root;
+    public:
+        PKBBuilder(TNode root) {
+            ast_root = root;
+        }
+
+        PKB build() {}
+};
