4180f1d1d7ef3dba163dd98d0dedc9bf7432c0af
Ho Zong Sien <hozongsien@gmail.com>
2021-02-08 07:06:51 +0000

    Add basic Tree Node

--- Team07/Code07/src/spa/src/TNode.cpp
+++ Team07/Code07/src/spa/src/TNode.cpp
@@ -1,1 +1,28 @@
 #include "TNode.h"
+
+TNode::TNode()
+{
+  value = "";
+  type = "";
+}
+
+TNode::TNode(std::string val, std::string typ)
+{
+  value = val;
+  type = typ;
+}
+
+std::string TNode::getValue()
+{
+  return value;
+}
+
+std::string TNode::getType()
+{
+  return type;
+}
+
+std::vector<TNode> TNode::getChildren()
+{
+  return children;
+}
--- Team07/Code07/src/spa/src/TNode.h
+++ Team07/Code07/src/spa/src/TNode.h
@@ -1,5 +1,20 @@
+#include <string>
+#include <vector>
+
 class TNode
 {
-public:
+  /* value:type */
+private:
+  std::string value; // proc/var names, const, then, else, stmt number
+  std::string type;  // var, const, call, assign, if, while, operators, stmtLst
 
+  std::vector<TNode> children;
+
+public:
+  TNode();
+  TNode(std::string val, std::string type);
+
+  std::string getValue();
+  std::string getType();
+  std::vector<TNode> getChildren();
 };
