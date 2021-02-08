aa788ada3b8195c416379af3b5ee225fcb178aaf
yapdianhao <dianhao.yap@2359media.com>
2021-02-08 20:20:27 +0800

    add stub for PKBQueryController

---
+++ Team07/Code07/src/spa/src/PKBQueryController.cpp
@@ -0,0 +1,60 @@
+#include <vector>
+#include "PKB.hpp"
+#include "PKBQueryController.hpp"
+
+PKBQueryController::PKBQueryController() {
+
+}
+
+bool PKBQueryController::isFollows(stmt_ref s1, stmt_ref s2) {
+    return true;
+}
+
+bool PKBQueryController::isFollowsStar(stmt_ref s1, stmt_ref s2) {
+    return true;
+}
+
+bool PKBQueryController::isParent(stmt_ref s1, stmt_ref s2) {
+    return true;
+}
+
+bool PKBQueryController::isParentStar(stmt_ref s1, stmt_ref s2) {
+    return true;
+}
+
+bool PKBQueryController::statementUses(stmt_ref s, var_ref v) {
+    return true;
+}
+
+bool PKBQueryController::statementModifies(stmt_ref s, var_ref v) {
+    return true;
+}
+
+bool PKBQueryController::satisfiesPattern(assign_ref a, pattern p) {
+    return true;
+}
+
+std::vector<constant> PKBQueryController::getAllConstants() {
+    std::vector<constant> vect;
+    return vect;
+}
+
+std::vector<proc_ref> PKBQueryController::getAllProcedures() {
+    std::vector<proc_ref> vect;
+    return vect;
+}
+
+std::vector<statement> PKBQueryController::getAllStatements() {
+    std::vector<statement> vect;
+    return vect;
+}
+
+std::vector<var_ref> PKBQueryController::getAllVariables() {
+    std::vector<var_ref> vect;
+    return vect;
+}
+
+std::vector<stmt_ref> PKBQueryController::getStatementsOfType(stmt_type t) {
+    std::vector<stmt_ref> vect;
+    return vect;
+}
