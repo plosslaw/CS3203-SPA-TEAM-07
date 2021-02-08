9cc21acc14f46fa8b91c4240f4149ac6836820bc
plosslaw <spacequartz@gmail.com>
2021-02-08 18:23:37 +0800

    Update Action.cpp constructor parameters

--- Team07/Code07/src/spa/src/Action.cpp
+++ Team07/Code07/src/spa/src/Action.cpp
@@ -1,10 +1,6 @@
-class Action {
-public: 
-    Action() {
+#include "Action.h"
+
+Action::Action(search_type searchType, std::string firstArg, std::string secondArg,
+    arg_type firstArgType, arg_type secondArgType) {
         
     }
-
-private:
-
-
-};
--- Team07/Code07/src/spa/src/QueryEvaluator.cpp
+++ Team07/Code07/src/spa/src/QueryEvaluator.cpp
@@ -2,10 +2,6 @@
 
 using namespace std;
 
-Action::Action() {
-    
-}
-
 void QEExecuteQuery(vector<Action>) {
 
 }
