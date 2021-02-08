7a8e5636f79541f4aa423cb39c01abd8d75a3487
AHaliq <liqo12@hotmail.com>
2021-02-08 19:57:24 +0800

    implement api for parser

--- Team07/Code07/src/spa/src/Parser.cpp
+++ Team07/Code07/src/spa/src/Parser.cpp
@@ -7,38 +7,11 @@
 
 //using namespace std;
 
-#include "PKB.h"
-#include "TNode.h"
 
-template<typename T>
-class ParseState {
-		T a;
-		int o;
-		std::string* str;
-	public:
-		ParseState<T>(ParseState<T> &p) {
-			str = p.str;
-			o = p.o;
-			a = p.a;
-		}
-		ParseState(std::string &s) {
-			str = &s;
-			o = 0;
-		}
-		char head() {
-			return (*str).at(o++);
-		}
-		bool end() {
-			return (*str).length() == o;
-		}
-};
-
-int Parse (string file) {
+int Parse (std::string file) {
 	std::ifstream t(file);
 	std::string str((std::istreambuf_iterator<char>(t)),
                  std::istreambuf_iterator<char>());
-	ParseState<char> p(str);
-	cout<<p.head()<<"...\n\n\n";
-	cout<<str<<"\n\n\n\n";
+	std::cout << str <<"\n\n\n\n";
 	return 0;
 }
