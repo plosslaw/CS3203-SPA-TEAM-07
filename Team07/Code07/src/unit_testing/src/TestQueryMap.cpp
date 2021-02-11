f625b29232fed3ec7d1346e893b094c55085c79d
Ho Zong Sien <hozongsien@gmail.com>
2021-02-11 12:55:07 +0000

    Add unit tests

--- Team07/Code07/src/unit_testing/src/TestQueryMap.cpp
+++ Team07/Code07/src/unit_testing/src/TestQueryMap.cpp
@@ -98,3 +98,138 @@ TEST_CASE("PayLoad with pair variable") {
     REQUIRE(synonym_assign.getValue() == "_,_");
   }
 }
+
+TEST_CASE("PQL with no such that and no pattern") {
+  /*
+  variable v;
+  Select v
+  */
+  QueryMap query;
+  query.addItem(DECLARATION, PayLoad(SINGLE, VARIABLE, "v"));
+  query.addItem(SELECT, PayLoad(SINGLE, SYNONYM, "v"));
+
+  REQUIRE(query.getList(DECLARATION).size() == 1);
+  REQUIRE(query.getList(SELECT).size() == 1);
+  REQUIRE(query.getList(SUCHTHAT).empty());
+  REQUIRE(query.getList(PATTERN).empty());
+
+  SECTION("Declaration clause") {
+    REQUIRE(query.getList(DECLARATION)[0] == PayLoad(SINGLE, VARIABLE, "v"));
+
+    REQUIRE_FALSE(query.getList(DECLARATION)[0] ==
+                  PayLoad(PAIR, VARIABLE, "v"));
+    REQUIRE_FALSE(query.getList(DECLARATION)[0] ==
+                  PayLoad(SINGLE, VARIABLE, "s"));
+    REQUIRE_FALSE(query.getList(DECLARATION)[0] ==
+                  PayLoad(SINGLE, STATEMENT, "s"));
+  }
+
+  SECTION("Select clause") {
+    REQUIRE(query.getList(SELECT)[0] == PayLoad(SINGLE, SYNONYM, "v"));
+
+    REQUIRE_FALSE(query.getList(SELECT)[0] == PayLoad(PAIR, SYNONYM, "v"));
+    REQUIRE_FALSE(query.getList(SELECT)[0] == PayLoad(SINGLE, VARIABLE, "v"));
+    REQUIRE_FALSE(query.getList(SELECT)[0] == PayLoad(SINGLE, SYNONYM, "s"));
+  }
+
+  SECTION("Such that clause") {}
+
+  SECTION("Pattern clause") {}
+}
+
+TEST_CASE("PQL with one such that") {
+  /*
+  stmt s;
+  Select s such that Follows* (6, s)
+  */
+  QueryMap query;
+  query.addItem(DECLARATION, PayLoad(SINGLE, STATEMENT, "s"));
+  query.addItem(SELECT, PayLoad(SINGLE, SYNONYM, "s"));
+  query.addItem(SUCHTHAT, PayLoad(PAIR, FOLLOWST, "6,s"));
+
+  REQUIRE(query.getList(DECLARATION).size() == 1);
+  REQUIRE(query.getList(SELECT).size() == 1);
+  REQUIRE(query.getList(SUCHTHAT).size() == 1);
+  REQUIRE(query.getList(PATTERN).empty());
+
+  SECTION("Declaration clause") {
+    REQUIRE(query.getList(DECLARATION)[0] == PayLoad(SINGLE, STATEMENT, "s"));
+  }
+
+  SECTION("Select clause") {
+    REQUIRE(query.getList(SELECT)[0] == PayLoad(SINGLE, SYNONYM, "s"));
+  }
+
+  SECTION("Such that clause") {
+    REQUIRE(query.getList(SUCHTHAT)[0] == PayLoad(PAIR, FOLLOWST, "6,s"));
+  }
+
+  SECTION("Pattern clause") {}
+}
+
+TEST_CASE("PQL with one pattern") {
+  /*
+  assign a;
+  Select a pattern a (_,“count + 1”)
+  */
+  QueryMap query;
+  query.addItem(DECLARATION, PayLoad(SINGLE, ASSIGN, "a"));
+  query.addItem(SELECT, PayLoad(SINGLE, SYNONYM, "a"));
+  query.addItem(PATTERN, PayLoad(PAIR, SYN_ASSIGN, "_,count + 1"));
+
+  REQUIRE(query.getList(DECLARATION).size() == 1);
+  REQUIRE(query.getList(SELECT).size() == 1);
+  REQUIRE(query.getList(SUCHTHAT).empty());
+  REQUIRE(query.getList(PATTERN).size() == 1);
+
+  SECTION("Declaration clause") {
+    REQUIRE(query.getList(DECLARATION)[0] == PayLoad(SINGLE, ASSIGN, "a"));
+  }
+
+  SECTION("Select clause") {
+    REQUIRE(query.getList(SELECT)[0] == PayLoad(SINGLE, SYNONYM, "a"));
+  }
+
+  SECTION("Such that clause") {}
+
+  SECTION("Pattern clause") {
+    REQUIRE(query.getList(PATTERN)[0] ==
+            PayLoad(PAIR, SYN_ASSIGN, "_,count + 1"));
+  }
+}
+
+TEST_CASE("PQL with one such that and one pattern") {
+  /*
+  assign a; while w;
+  Select w such that Parent* (w, a) pattern a (“count”, _)
+  */
+
+  QueryMap query;
+  query.addItem(DECLARATION, PayLoad(SINGLE, ASSIGN, "a"));
+  query.addItem(DECLARATION, PayLoad(SINGLE, WHILE, "w"));
+  query.addItem(SELECT, PayLoad(SINGLE, SYNONYM, "w"));
+  query.addItem(SUCHTHAT, PayLoad(PAIR, PARENTT, "w,a"));
+  query.addItem(PATTERN, PayLoad(PAIR, SYN_ASSIGN, "count,_"));
+
+  REQUIRE(query.getList(SELECT).size() == 1);
+  REQUIRE(query.getList(DECLARATION).size() == 2);
+  REQUIRE(query.getList(SUCHTHAT).size() == 1);
+  REQUIRE(query.getList(PATTERN).size() == 1);
+
+  SECTION("Declaration clause") {
+    REQUIRE(query.getList(DECLARATION)[0] == PayLoad(SINGLE, ASSIGN, "a"));
+    REQUIRE(query.getList(DECLARATION)[1] == PayLoad(SINGLE, WHILE, "w"));
+  }
+
+  SECTION("Select clause") {
+    REQUIRE(query.getList(SELECT)[0] == PayLoad(SINGLE, SYNONYM, "w"));
+  }
+
+  SECTION("Such that clause") {
+    REQUIRE(query.getList(SUCHTHAT)[0] == PayLoad(PAIR, PARENTT, "w,a"));
+  }
+
+  SECTION("Pattern clause") {
+    REQUIRE(query.getList(PATTERN)[0] == PayLoad(PAIR, SYN_ASSIGN, "count,_"));
+  }
+}
