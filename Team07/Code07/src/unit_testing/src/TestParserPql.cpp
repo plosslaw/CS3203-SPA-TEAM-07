eba46b33c283d5b1130d2fb33260a07f90027e67
Ho Zong Sien <hozongsien@gmail.com>
2021-02-16 10:07:26 +0000

    Add unit test for multiple declarations of different entity types

--- Team07/Code07/src/unit_testing/src/TestParserPql.cpp
+++ Team07/Code07/src/unit_testing/src/TestParserPql.cpp
@@ -3,6 +3,61 @@
 #include <iostream>
 #include <vector>
 
+TEST_CASE("QueryMap declaration") {
+  SECTION("Multi single groups") {
+    std::string query = "while w1; stmt s1;";
+    QueryMap expectedQueryMap;
+    expectedQueryMap.addItem(
+        DECLARATION, PayLoad(SINGLE, WHILE, std::vector<std::string>{"w1"}));
+    expectedQueryMap.addItem(
+        DECLARATION,
+        PayLoad(SINGLE, STATEMENT, std::vector<std::string>{"s1"}));
+
+    QueryMap actualQueryMap = pqlParse(query);
+    REQUIRE(expectedQueryMap == actualQueryMap);
+  }
+
+  SECTION("Single multiple group") {
+    std::string query = "while w1, w2;";
+    QueryMap expectedQueryMap;
+    expectedQueryMap.addItem(
+        DECLARATION, PayLoad(SINGLE, WHILE, std::vector<std::string>{"w1"}));
+    expectedQueryMap.addItem(
+        DECLARATION, PayLoad(SINGLE, WHILE, std::vector<std::string>{"w2"}));
+
+    QueryMap actualQueryMap = pqlParse(query);
+    REQUIRE(expectedQueryMap == actualQueryMap);
+  }
+
+  SECTION("Multi multiple groups") {
+    std::string query = "while w1, w2; stmt s1, s2;";
+    QueryMap expectedQueryMap;
+    expectedQueryMap.addItem(
+        DECLARATION, PayLoad(SINGLE, WHILE, std::vector<std::string>{"w1"}));
+    expectedQueryMap.addItem(
+        DECLARATION, PayLoad(SINGLE, WHILE, std::vector<std::string>{"w2"}));
+    expectedQueryMap.addItem(
+        DECLARATION,
+        PayLoad(SINGLE, STATEMENT, std::vector<std::string>{"s1"}));
+    expectedQueryMap.addItem(
+        DECLARATION,
+        PayLoad(SINGLE, STATEMENT, std::vector<std::string>{"s2"}));
+
+    QueryMap actualQueryMap = pqlParse(query);
+    REQUIRE(expectedQueryMap == actualQueryMap);
+  }
+}
+
+TEST_CASE("QueryMap with no declaration, no such that, no pattern") {
+  std::string query = "Select w";
+  QueryMap expectedQueryMap;
+  expectedQueryMap.addItem(
+      SELECT, PayLoad(SINGLE, SYNONYM, std::vector<std::string>{"w"}));
+
+  QueryMap actualQueryMap = pqlParse(query);
+  REQUIRE(expectedQueryMap == actualQueryMap);
+}
+
 TEST_CASE("QueryMap with no such that and no pattern") {
   SECTION("while") {
     std::string query = "while w; Select w";
@@ -78,7 +133,7 @@ TEST_CASE("QueryMap repeated declaration with no such that and no pattern") {
   }
 
   SECTION("read") {
-    std::string query = "read re1, re2,re3; Select re1";
+    std::string query = "read re1, re2, re3; Select re1";
     QueryMap expectedQueryMap;
     expectedQueryMap.addItem(
         DECLARATION, PayLoad(SINGLE, READ, std::vector<std::string>{"re1"}));
@@ -95,7 +150,27 @@ TEST_CASE("QueryMap repeated declaration with no such that and no pattern") {
 }
 
 // TODO(zs)
-TEST_CASE("QueryMap with one such that") {}
+// TEST_CASE("QueryMap with one such that") {
+//   SECTION("Parent") {
+//     std::string query = "stmt s1, s2; Select s1 such that Parent(s1, s2)";
+//     QueryMap expectedQueryMap;
+//     expectedQueryMap.addItem(
+//         DECLARATION,
+//         PayLoad(SINGLE, STATEMENT, std::vector<std::string>{"s1"}));
+//     expectedQueryMap.addItem(
+//         DECLARATION,
+//         PayLoad(SINGLE, STATEMENT, std::vector<std::string>{"s2"}));
+//     expectedQueryMap.addItem(
+//         SELECT,
+//         PayLoad(SINGLE, SYNONYM, std::vector<std::string>{"s1"}));
+//     expectedQueryMap.addItem(
+//         SUCHTHAT,
+//         PayLoad(PAIR, PARENT, std::vector<std::string>{"s1", "s2"}));
+
+//     QueryMap actualQueryMap = pqlParse(query);
+//     REQUIRE(expectedQueryMap == actualQueryMap);
+//   }
+// }
 // TODO(zs)
 TEST_CASE("QueryMap with one pattern") {}
 // TODO(zs)
