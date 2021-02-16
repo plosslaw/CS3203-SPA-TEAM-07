79c34979af8af930185288d14c97172dce293144
plosslaw <spacequartz@gmail.com>
2021-02-16 11:52:02 +0800

    Add basic ActionsExecutor api

--- Team07/Code07/src/spa/src/ActionsExecutor.h
+++ Team07/Code07/src/spa/src/ActionsExecutor.h
@@ -1,25 +1,50 @@
 #pragma once
 
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 #include "Action.h"
 #include "PKB.h"
 #include "PKBQueryController.hpp"
-#include "ResponseRenderer.h"
+#include "QueryResult.h"
+
 
 class ActionsExecutor {
 public:
+    enum stmt_pos {
+        FIRST_ARG,
+        SECOND_ARG
+    };
+
     ActionsExecutor();
     explicit ActionsExecutor(PKBQueryController pkbQueryController);
-    std::vector<std::string> executeActions(std::vector<Action> queryActions);
+
+    // Basic api
+    std::vector<QueryResult> getAllConstants();
+    std::vector<QueryResult> getAllEntitiesOfType(stmt_type entityType);
+    std::vector<QueryResult> getAllProcedures();
+    std::vector<QueryResult> getAllStatements();
+    std::vector<QueryResult> getAllVariables();
+
+    // Single clause api
+    std::vector<QueryResult> getAllStmtFollows(stmt_type stmtType, stmt_pos stmtPos);
+    std::vector<QueryResult> getAllStmtFollows(stmt_type stmtType, stmt_pos stmtPos, stmt_ref otherStmt);
+    std::vector<QueryResult> getAllStmtFollows(stmt_type stmtType, stmt_pos stmtPos, stmt_type otherStmtType);
+    
+    std::vector<QueryResult> getAllStmtParent(stmt_type stmtType);
+
+    std::vector<QueryResult> getAllStmtModifies(var_ref v);
+    std::vector<QueryResult> getAllStmtUses(var_ref v);
+
+    std::vector<QueryResult> getAllVariableModifies(stmt_ref stmt);
+    std::vector<QueryResult> getAllVariableUses(stmt_ref stmt);
+
+    std::vector<QueryResult> getAllProcedureModifies(var_ref v);
+    std::vector<QueryResult> getAllProcedureUses(var_ref v);
+
+    // std::vector<std::string> executeActions(std::vector<Action> queryActions);
 
 private:
-    void declareSynonyms();
-    void createDependencyGraph();
-    std::vector<std::string> getAllThatSatisfies();
-
     PKBQueryController pkbQueryController;
 
 };
