f5473d8d6a6ca883bb0ca94fa4e5b03d7575f4d7
Ho Zong Sien <hozongsien@gmail.com>
2021-02-16 01:54:14 +0000

    Add support for Single repeated design entity declaration and whitespace invariant in PQL comand

--- Team07/Code07/src/spa/src/ParserPql.cpp
+++ Team07/Code07/src/spa/src/ParserPql.cpp
@@ -6,10 +6,8 @@
 std::string ident(State &state) {
   int init = state.i;
   try {
-    char r1 = charPredicate(state, &alphaPred, "letter");
-    // :- letter
-    std::string r2 = alphaNum(state);
-    // :- (letter | digit)*
+    char r1 = charPredicate(state, &alphaPred, "letter"); // :- letter
+    std::string r2 = alphaNum(state); // :- (letter | digit)*
     r2.insert(r2.begin(), r1);
     return r2;
   } catch (ParseException &e) {
@@ -24,32 +22,45 @@ PayLoad declaration(State &state, std::string design_entity, Single load_type) {
     stringMatch(state, design_entity);
     whitespace(state);
     std::string synonym = ident(state);
+    whitespace(state);
+    so.assign(state);
     return PayLoad(SINGLE, load_type, synonym);
   } catch (ParseException &e) {
+    State so1(state);
+    state.assign(so);
     state.excps.push_back(e);
     throw ParseException(so.i, state.i, "declaration", "");
   }
 }
 
-PayLoad stmt_decl(State &state) {
-  return declaration(state, "stmt", STATEMENT);
-}
-PayLoad read_decl(State &state) { return declaration(state, "read", READ); }
-PayLoad print_decl(State &state) { return declaration(state, "print", PRINT); }
-PayLoad call_decl(State &state) { return declaration(state, "call", CALL); }
-PayLoad while_decl(State &state) { return declaration(state, "while", WHILE); }
-PayLoad if_decl(State &state) { return declaration(state, "if", IF); }
-PayLoad assign_decl(State &state) {
-  return declaration(state, "assign", ASSIGN);
-}
-PayLoad variable_decl(State &state) {
-  return declaration(state, "variable", VARIABLE);
-}
-PayLoad constant_decl(State &state) {
-  return declaration(state, "constant", CONSTANT);
-}
-PayLoad procedure_decl(State &state) {
-  return declaration(state, "procedure", PROCEDURE);
+std::vector<PayLoad> repeat_declaration(State &state, std::string design_entity,
+                                        Single load_type) {
+  State so(state);
+  std::vector<PayLoad> declarations;
+
+  try {
+    declarations.push_back(declaration(state, design_entity, load_type));
+    so.assign(state);
+  } catch (ParseException &e) {
+    throw e;
+  }
+  try {
+    while (true) {
+      declarations.push_back(declaration(state, ",", load_type));
+      so.assign(state);
+    }
+  } catch (ParseException &e) {
+    State so1(state);
+    state.assign(so);
+    try {
+      stringMatch(state, ";");
+      whitespace(state);
+    } catch (ParseException &e) {
+      state.assign(so1);
+      throw e;
+    }
+  }
+  return declarations;
 }
 
 std::vector<PayLoad> declaration_cl(State &state) {
@@ -59,54 +70,74 @@ std::vector<PayLoad> declaration_cl(State &state) {
   State so(state);
   std::vector<PayLoad> declarations;
   try {
-    PayLoad decl_payload = stmt_decl(state);
-    declarations.push_back(decl_payload);
-    stringMatch(state, ";");
+    std::vector<PayLoad> while_decl = repeat_declaration(state, "while", WHILE);
+    declarations.insert(declarations.end(), while_decl.begin(),
+                        while_decl.end());
   } catch (ParseException &e) {
+    state.assign(so);
     try {
-      PayLoad decl_payload = read_decl(state);
-      declarations.push_back(decl_payload);
-      stringMatch(state, ";");
+      std::vector<PayLoad> stmt_decl =
+          repeat_declaration(state, "stmt", STATEMENT);
+      declarations.insert(declarations.end(), stmt_decl.begin(),
+                          stmt_decl.end());
     } catch (ParseException &e) {
+      state.assign(so);
       try {
-        PayLoad decl_payload = print_decl(state);
-        declarations.push_back(decl_payload);
-        stringMatch(state, ";");
+        std::vector<PayLoad> read_decl =
+            repeat_declaration(state, "read", READ);
+        declarations.insert(declarations.end(), read_decl.begin(),
+                            read_decl.end());
       } catch (ParseException &e) {
+        state.assign(so);
         try {
-          PayLoad decl_payload = call_decl(state);
-          declarations.push_back(decl_payload);
-          stringMatch(state, ";");
+          std::vector<PayLoad> print_decl =
+              repeat_declaration(state, "print", PRINT);
+          declarations.insert(declarations.end(), print_decl.begin(),
+                              print_decl.end());
         } catch (ParseException &e) {
+          state.assign(so);
           try {
-            PayLoad decl_payload = while_decl(state);
-            declarations.push_back(decl_payload);
-            stringMatch(state, ";");
+            std::vector<PayLoad> call_decl =
+                repeat_declaration(state, "call", CALL);
+            declarations.insert(declarations.end(), call_decl.begin(),
+                                call_decl.end());
           } catch (ParseException &e) {
+            state.assign(so);
             try {
-              PayLoad decl_payload = if_decl(state);
-              declarations.push_back(decl_payload);
-              stringMatch(state, ";");
+              std::vector<PayLoad> if_decl =
+                  repeat_declaration(state, "if", IF);
+              declarations.insert(declarations.end(), if_decl.begin(),
+                                  if_decl.end());
             } catch (ParseException &e) {
+              state.assign(so);
               try {
-                PayLoad decl_payload = assign_decl(state);
-                declarations.push_back(decl_payload);
-                stringMatch(state, ";");
+                std::vector<PayLoad> assign_decl =
+                    repeat_declaration(state, "assign", ASSIGN);
+                declarations.insert(declarations.end(), assign_decl.begin(),
+                                    assign_decl.end());
               } catch (ParseException &e) {
+                state.assign(so);
                 try {
-                  PayLoad decl_payload = variable_decl(state);
-                  declarations.push_back(decl_payload);
-                  stringMatch(state, ";");
+                  std::vector<PayLoad> variable_decl =
+                      repeat_declaration(state, "variable", VARIABLE);
+                  declarations.insert(declarations.end(), variable_decl.begin(),
+                                      variable_decl.end());
                 } catch (ParseException &e) {
+                  state.assign(so);
                   try {
-                    PayLoad decl_payload = constant_decl(state);
-                    declarations.push_back(decl_payload);
-                    stringMatch(state, ";");
+                    std::vector<PayLoad> constant_decl =
+                        repeat_declaration(state, "constant", CONSTANT);
+                    declarations.insert(declarations.end(),
+                                        constant_decl.begin(),
+                                        constant_decl.end());
                   } catch (ParseException &e) {
+                    state.assign(so);
                     try {
-                      PayLoad decl_payload = procedure_decl(state);
-                      declarations.push_back(decl_payload);
-                      stringMatch(state, ";");
+                      std::vector<PayLoad> procedure_decl =
+                          repeat_declaration(state, "procedure", PROCEDURE);
+                      declarations.insert(declarations.end(),
+                                          procedure_decl.begin(),
+                                          procedure_decl.end());
                     } catch (ParseException &e) {
                       throw e;
                     }
@@ -143,7 +174,7 @@ QueryMap pqlParse(std::string query) {
   QueryMap queryMap;
 
   // State state(&query);
-  std::string str = "while w1;Select w1";
+  std::string str = "while w1, w2, w3; Select w1";
   State state(&str);
 
   try {
@@ -151,8 +182,11 @@ QueryMap pqlParse(std::string query) {
     std::vector<PayLoad> selects = select_cl(state);
 
     std::vector<PayLoad> suchthats; // TODO(zs)
-    std::vector<PayLoad> patterns; // TODO(zs)
+    std::vector<PayLoad> patterns;  // TODO(zs)
     std::cout << declarations[0].getValue() << std::endl;
+    std::cout << declarations[1].getValue() << std::endl;
+    std::cout << declarations[2].getValue() << std::endl;
+
     std::cout << selects[0].getValue() << std::endl;
 
     return QueryMap(declarations, selects, suchthats, patterns);
