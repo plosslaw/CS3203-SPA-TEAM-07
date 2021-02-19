3c432bb6a4719261dfcc61e65e0c2472fe37d703
AHaliq <liqo12@hotmail.com>
2021-02-19 03:54:01 +0000

    fix out of bounds binary search bug

--- Team07/Code07/src/spa/src/ParserIndexMapper.cpp
+++ Team07/Code07/src/spa/src/ParserIndexMapper.cpp
@@ -11,12 +11,13 @@ ParserMapper::ParserMapper(std::string *str) {
     char c = (*str).at(i);
     if(c == '\n') {
       line_start.push_back(i + 1);
-      lines.push_back(std::string(linebuilder.begin(), linebuilder.end()));
+      lines.push_back(linebuilder.size() > 0 ? std::string(linebuilder.begin(), linebuilder.end()) : "");
       linebuilder.clear();
     } else {
       linebuilder.push_back(c);
     }
   }
+  lines.push_back(linebuilder.size() > 0 ? std::string(linebuilder.begin(), linebuilder.end()) : "");
 }
 
 // binary search to find line
@@ -25,14 +26,15 @@ int ParserMapper::get_line(int i) {
   int l = 0;
   while (r >= l) {
     int mid = l + (r - l) / 2;
-    if (line_start[mid] == i) return mid;
+    if (line_start[mid] == i || mid == line_start.size() - 1) return mid;
     if (line_start[mid] > i) {
       r = mid - 1;
     } else {
       l = mid + 1;
     }
   }
-  return r >= 0 && line_start[r] < i ? r : l;
+  int res = r >= 0 && line_start[r] < i ? r : l;
+  return res;
 }
 
 int ParserMapper::get_col(int line, int i) {
@@ -50,7 +52,7 @@ std::string ParserMapper::get_caret(int col) {
 std::string ParserMapper::get_line_caret(int i, int sep) {
   int line = get_line(i);
   int col = get_col(line, i);
-  std::string linenum(std::to_string(line) + "| ");
+  std::string linenum(std::to_string(line + 1) + "| ");
   std::string sepstr(sep, ' ');
   return sepstr + linenum + get_str(line) + "\n" + sepstr + std::string(linenum.size(), ' ') + get_caret(col) + "\n";
 }
