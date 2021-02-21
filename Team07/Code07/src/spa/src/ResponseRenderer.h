d6fde6e2a2d534cfa4c8c5e44e8716a88714479f
plosslaw <spacequartz@gmail.com>
2021-02-21 18:36:03 +0800

    Add sorting to ResponseRenderer

--- Team07/Code07/src/spa/src/ResponseRenderer.h
+++ Team07/Code07/src/spa/src/ResponseRenderer.h
@@ -1,14 +1,18 @@
 #pragma once
 
+#include <algorithm>
 #include <string>
+#include <unordered_set>
 #include <vector>
 
 class ResponseRenderer {
 public:
     explicit ResponseRenderer();
 
-    std::vector<std::string> render_results(std::vector<std::string>);
+    std::vector<std::string> render_results(std::vector<std::string> query_results);
 
 private:
-    bool check_if_results_integer(std::vector<std::string>);
+    static bool compare_int_strings(std::string s1, std::string s2);
+    static bool check_is_number(std::string s);
+    std::unordered_set<int> digits_set;
 };
