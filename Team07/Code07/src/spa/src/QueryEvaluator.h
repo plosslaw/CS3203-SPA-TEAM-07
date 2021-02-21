33d00f3f73b9ab6b67260c7a0eb1b23ff8b86cb2
plosslaw <spacequartz@gmail.com>
2021-02-21 10:19:59 +0800

    Change camelCase to snake_case

--- Team07/Code07/src/spa/src/QueryEvaluator.h
+++ Team07/Code07/src/spa/src/QueryEvaluator.h
@@ -14,11 +14,11 @@ class QueryEvaluator {
 public:
     explicit QueryEvaluator();
     explicit QueryEvaluator(PKBQueryController pkb_query_controller);
-    std::vector<std::string> QERunQuery(QueryMap query_map);
+    std::vector<std::string> run_query(QueryMap query_map);
 
 private:
-    std::vector<std::string> QEGenerateActions(QueryMap query_map);
-    std::vector<std::string> QERenderResults(std::vector<std::string> queryResults);
+    std::vector<std::string> generate_actions(QueryMap query_map);
+    std::vector<std::string> render_results(std::vector<std::string> query_results);
 
     ActionsExecutor actions_executor;
 
