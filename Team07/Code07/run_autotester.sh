5133ea8eb99c58a3d09794b7f42b6752ccc5d3e8
yapdianhao <dianhao.yap@2359media.com>
2021-02-21 23:26:22 +0000

    added some queries for testcase 3

--- Team07/Code07/run_autotester.sh
+++ Team07/Code07/run_autotester.sh
@@ -1,3 +1,3 @@
 ./build/src/unit_testing/unit_testing
 ./build/src/integration_testing/integration_testing
-./build/src/autotester/autotester tests/testcase2_source.txt tests/testcase2_queries.txt tests/out.xml
+./build/src/autotester/autotester $1 $2 $3
